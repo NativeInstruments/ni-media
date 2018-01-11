44d9df955c6a1f659cb5021ea8fff0f222a35e33
Henry Hoegelow <henry@hoegelow.com>
2018-01-11 08:12:25 +0100

    fix formatting (apply clang-format)

--- audiostream/src/ni/media/audio/source/gstreamer_file_source.cpp
+++ audiostream/src/ni/media/audio/source/gstreamer_file_source.cpp
@@ -30,300 +30,307 @@
 #include <boost/format.hpp>
 #include <boost/make_unique.hpp>
 
+#include <algorithm>
 #include <codecvt>
 #include <functional>
-#include <locale>
 #include <iostream>
-#include <algorithm>
+#include <locale>
 
 #include <gst/app/gstappsink.h>
 
 namespace detail
 {
-  template<typename T, size_t s>
-    struct RingBuffer
+template <typename T, size_t s>
+struct RingBuffer
+{
+    static constexpr size_t size = s;
+    static constexpr size_t mask = ( s - 1 );
+    std::array<T, s> m_buffer;
+    uint64_t m_write_head = 0;
+    uint64_t m_read_head  = 0;
+
+    void push( const T* data, size_t count )
     {
-        static constexpr size_t size = s;
-        static constexpr size_t mask = (s - 1);
-        std::array<T, s> m_buffer;
-        uint64_t m_write_head = 0;
-        uint64_t m_read_head = 0;
+        auto filled = m_write_head - m_read_head;
+        auto space  = s - filled;
+        assert( count <= space );
 
-        void push(const T* data, size_t count)
+        if ( count )
         {
-          auto filled = m_write_head - m_read_head;
-          auto space = s - filled;
-          assert(count <= space);
-
-          if(count)
-          {
-            auto idx = m_write_head & mask;
-            auto space = size - idx;
-            auto for_now = std::min(space, count);
-            std::copy(data, data + for_now, m_buffer.data() + idx);
+            auto idx     = m_write_head & mask;
+            auto space   = size - idx;
+            auto for_now = std::min( space, count );
+            std::copy( data, data + for_now, m_buffer.data() + idx );
             m_write_head += for_now;
-            push(data + for_now, count - for_now);
-          }
+            push( data + for_now, count - for_now );
         }
+    }
 
-        size_t pull(T* data, size_t count)
+    size_t pull( T* data, size_t count )
+    {
+        count = std::min( count, m_write_head - m_read_head );
+
+        if ( count )
         {
-          count = std::min(count, m_write_head - m_read_head);
-
-          if(count)
-          {
-            auto idx = m_read_head & mask;
-            auto space = size - idx;
-            auto for_now = std::min(space, count);
-            std::copy(m_buffer.data() + idx, m_buffer.data() + idx + for_now, data);
+            auto idx     = m_read_head & mask;
+            auto space   = size - idx;
+            auto for_now = std::min( space, count );
+            std::copy( m_buffer.data() + idx, m_buffer.data() + idx + for_now, data );
             m_read_head += for_now;
-            return for_now + pull(data + for_now, count - for_now);
-          }
-          return 0;
+            return for_now + pull( data + for_now, count - for_now );
         }
+        return 0;
+    }
 
-        void flush()
-        {
-          m_read_head = m_write_head = 0;
-        }
-    };
+    void flush()
+    {
+        m_read_head = m_write_head = 0;
+    }
+};
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-gstreamer_file_source::gstreamer_file_source(const std::string& path, audio::ifstream_info::container_type container, size_t stream) :
-    m_pipeline(nullptr, gst_object_unref),
-    m_ring_buffer(new RingBuffer())
+gstreamer_file_source::gstreamer_file_source( const std::string&                   path,
+                                              audio::ifstream_info::container_type container,
+                                              size_t                               stream )
+: m_pipeline( nullptr, gst_object_unref )
+, m_ring_buffer( new RingBuffer() )
 {
-  init_gstreamer();
-  setup_source(path, container);
+    init_gstreamer();
+    setup_source( path, container );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
 gstreamer_file_source::~gstreamer_file_source()
 {
-  gst_element_set_state(m_pipeline.get(), GST_STATE_NULL);
-  wait_for_async_operation();
-  m_pipeline.reset();
+    gst_element_set_state( m_pipeline.get(), GST_STATE_NULL );
+    wait_for_async_operation();
+    m_pipeline.reset();
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
 GstState gstreamer_file_source::wait_for_async_operation()
 {
-  GstState current_state = GST_STATE_VOID_PENDING;
-  GstState pending_state = GST_STATE_VOID_PENDING;
+    GstState current_state = GST_STATE_VOID_PENDING;
+    GstState pending_state = GST_STATE_VOID_PENDING;
 
-  while(gst_element_get_state(m_pipeline.get(), &current_state, &pending_state, GST_MSECOND) == GST_STATE_CHANGE_ASYNC)
-  {
-    g_main_context_iteration(nullptr, FALSE);
-  }
+    while ( gst_element_get_state( m_pipeline.get(), &current_state, &pending_state, GST_MSECOND )
+            == GST_STATE_CHANGE_ASYNC )
+    {
+        g_main_context_iteration( nullptr, FALSE );
+    }
 
-  return current_state;
+    return current_state;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
 void gstreamer_file_source::init_gstreamer()
 {
-  static bool gstreamer_initialized = false;
+    static bool gstreamer_initialized = false;
 
-  if(!std::exchange(gstreamer_initialized, true))
-  {
-    gst_init(0, nullptr);
-  }
+    if ( !std::exchange( gstreamer_initialized, true ) )
+    {
+        gst_init( 0, nullptr );
+    }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-void gstreamer_file_source::setup_source(const std::string& path, audio::ifstream_info::container_type container)
+void gstreamer_file_source::setup_source( const std::string& path, audio::ifstream_info::container_type container )
 {
-  GstElement* sink = prepare_pipeline(path);
-  auto sinkpad = gst_element_get_static_pad(sink, "sink");
-  auto caps = gst_pad_get_current_caps(sinkpad);
-  auto caps_struct = gst_caps_get_structure(caps, 0);
-  fill_format_info(caps_struct, container);
+    GstElement* sink        = prepare_pipeline( path );
+    auto        sinkpad     = gst_element_get_static_pad( sink, "sink" );
+    auto        caps        = gst_pad_get_current_caps( sinkpad );
+    auto        caps_struct = gst_caps_get_structure( caps, 0 );
+    fill_format_info( caps_struct, container );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-GstElement* gstreamer_file_source::prepare_pipeline(const std::string& path)
+GstElement* gstreamer_file_source::prepare_pipeline( const std::string& path )
 {
-  m_pipeline.reset(gst_pipeline_new("pipeline"));
+    m_pipeline.reset( gst_pipeline_new( "pipeline" ) );
 
-  GstElement* source = gst_element_factory_make("filesrc", "source");
-  GstElement* decodebin = gst_element_factory_make("decodebin", "decoder");
-  GstElement* queue = gst_element_factory_make("queue", "queue");
-  GstElement* sink = gst_element_factory_make("appsink", "sink");
+    GstElement* source    = gst_element_factory_make( "filesrc", "source" );
+    GstElement* decodebin = gst_element_factory_make( "decodebin", "decoder" );
+    GstElement* queue     = gst_element_factory_make( "queue", "queue" );
+    GstElement* sink      = gst_element_factory_make( "appsink", "sink" );
 
-  gst_bin_add_many(GST_BIN(m_pipeline.get()), source, decodebin, queue, sink, nullptr);
-  gst_element_link_many(source, decodebin, NULL);
-  gst_element_link_many(queue, sink, NULL);
+    gst_bin_add_many( GST_BIN( m_pipeline.get() ), source, decodebin, queue, sink, nullptr );
+    gst_element_link_many( source, decodebin, NULL );
+    gst_element_link_many( queue, sink, NULL );
 
-  g_object_set(source, "location", path.c_str(), NULL);
+    g_object_set( source, "location", path.c_str(), NULL );
 
-  g_signal_connect(decodebin, "pad-added", G_CALLBACK(&onPadAdded), queue);
+    g_signal_connect( decodebin, "pad-added", G_CALLBACK( &onPadAdded ), queue );
 
-  preroll_pipeline();
-  return sink;
+    preroll_pipeline();
+    return sink;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
 void gstreamer_file_source::preroll_pipeline()
 {
-  gst_element_set_state(m_pipeline.get(), GST_STATE_PAUSED);
+    gst_element_set_state( m_pipeline.get(), GST_STATE_PAUSED );
 
-  if(wait_for_async_operation() != GST_STATE_PAUSED)
-    throw std::runtime_error( "gstreamer_file_source: pipeline doesn't preroll into paused state" );
+    if ( wait_for_async_operation() != GST_STATE_PAUSED )
+        throw std::runtime_error( "gstreamer_file_source: pipeline doesn't preroll into paused state" );
 
-  gst_element_set_state(m_pipeline.get(), GST_STATE_PLAYING);
+    gst_element_set_state( m_pipeline.get(), GST_STATE_PLAYING );
 
-  if(wait_for_async_operation() != GST_STATE_PLAYING)
-    throw std::runtime_error( "gstreamer_file_source: pipeline doesn't preroll into playing state" );
+    if ( wait_for_async_operation() != GST_STATE_PLAYING )
+        throw std::runtime_error( "gstreamer_file_source: pipeline doesn't preroll into playing state" );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-void gstreamer_file_source::fill_format_info(GstStructure *caps_struct, audio::ifstream_info::container_type container)
+void gstreamer_file_source::fill_format_info( GstStructure*                        caps_struct,
+                                              audio::ifstream_info::container_type container )
 {
-  m_info.container(container);
-  m_info.codec(audio::ifstream_info::codec_type::mp3);
-  m_info.lossless(false);
+    m_info.container( container );
+    m_info.codec( audio::ifstream_info::codec_type::mp3 );
+    m_info.lossless( false );
 
-  gint64 num_frames = 0;
-  gst_element_query_duration(m_pipeline.get(), GST_FORMAT_DEFAULT, &num_frames);
-  m_info.num_frames(num_frames);
+    gint64 num_frames = 0;
+    gst_element_query_duration( m_pipeline.get(), GST_FORMAT_DEFAULT, &num_frames );
+    m_info.num_frames( num_frames );
 
-  int sample_rate = 0;
-  gst_structure_get_int(caps_struct, "rate", &sample_rate);
-  m_info.sample_rate(sample_rate);
+    int sample_rate = 0;
+    gst_structure_get_int( caps_struct, "rate", &sample_rate );
+    m_info.sample_rate( sample_rate );
 
-  int num_channels = 0;
-  gst_structure_get_int(caps_struct, "channels", &num_channels);
-  m_info.num_channels(num_channels);
+    int num_channels = 0;
+    gst_structure_get_int( caps_struct, "channels", &num_channels );
+    m_info.num_channels( num_channels );
 
-  m_info.format(create_runtime_format(caps_struct));
+    m_info.format( create_runtime_format( caps_struct ) );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-pcm::runtime_format gstreamer_file_source::create_runtime_format(GstStructure* caps_struct)
+pcm::runtime_format gstreamer_file_source::create_runtime_format( GstStructure* caps_struct )
 {
-  const gchar* format = gst_structure_get_string(caps_struct, "format");
-  pcm::number_type number_type = gst_format_char_to_number_type(format[0]);
-  auto srcDepth = std::atol(format + 1);
-  auto endian = (strcmp(format, "BE") == 0) ? pcm::big_endian : pcm::little_endian;
-  return pcm::runtime_format(number_type, srcDepth, endian);
+    const gchar*     format      = gst_structure_get_string( caps_struct, "format" );
+    pcm::number_type number_type = gst_format_char_to_number_type( format[0] );
+    auto             srcDepth    = std::atol( format + 1 );
+    auto             endian      = ( strcmp( format, "BE" ) == 0 ) ? pcm::big_endian : pcm::little_endian;
+    return pcm::runtime_format( number_type, srcDepth, endian );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-pcm::number_type gstreamer_file_source::gst_format_char_to_number_type(const gchar format)
+pcm::number_type gstreamer_file_source::gst_format_char_to_number_type( const gchar format )
 {
-  if(format == 'U')
-    return pcm::unsigned_integer;
-  else if(format == 'F')
-    return pcm::floating_point;
+    if ( format == 'U' )
+        return pcm::unsigned_integer;
+    else if ( format == 'F' )
+        return pcm::floating_point;
 
-  return pcm::signed_integer;
+    return pcm::signed_integer;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-void gstreamer_file_source::onPadAdded(GstElement* element, GstPad* pad, GstElement* sink)
+void gstreamer_file_source::onPadAdded( GstElement* element, GstPad* pad, GstElement* sink )
 {
-  tGstPtr<GstPad> sinkpad(gst_element_get_static_pad(sink, "sink"), gst_object_unref);
-  gst_pad_link(pad, sinkpad.get());
+    tGstPtr<GstPad> sinkpad( gst_element_get_static_pad( sink, "sink" ), gst_object_unref );
+    gst_pad_link( pad, sinkpad.get() );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::streampos gstreamer_file_source::seek(offset_type off, BOOST_IOS::seekdir way )
+std::streampos gstreamer_file_source::seek( offset_type off, BOOST_IOS::seekdir way )
 {
-  assert( 0 == off % m_info.bytes_per_frame() );
+    assert( 0 == off % m_info.bytes_per_frame() );
 
-  int64_t newPosition = 0;
+    int64_t newPosition = 0;
 
-  if (way == BOOST_IOS::seekdir::_S_beg)
-  {
-    newPosition = off;
-  }
-  else if (way == BOOST_IOS::seekdir::_S_cur)
-  {
-    newPosition =  m_position + off;
-  }
-  else if (way == BOOST_IOS::seekdir::_S_end)
-  {
-    int64_t end = 0;
-    if(!gst_element_query_duration (m_pipeline.get(), GST_FORMAT_BYTES, &end))
+    if ( way == BOOST_IOS::seekdir::_S_beg )
     {
-      throw std::runtime_error( "gstreamer_file_source: seeking from end of file impossible - could not query duration" );
+        newPosition = off;
     }
-    newPosition = end + off;
-  }
-
-  if(m_position != newPosition)
-  {
-    m_position = newPosition;
-    m_ring_buffer->flush();
-
-    if(!gst_element_seek_simple(m_pipeline.get(), GST_FORMAT_BYTES, (GstSeekFlags)(GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_ACCURATE), m_position))
+    else if ( way == BOOST_IOS::seekdir::_S_cur )
     {
-      throw std::runtime_error( "gstreamer_file_source: seeking failed" );
+        newPosition = m_position + off;
     }
-  }
+    else if ( way == BOOST_IOS::seekdir::_S_end )
+    {
+        int64_t end = 0;
+        if ( !gst_element_query_duration( m_pipeline.get(), GST_FORMAT_BYTES, &end ) )
+        {
+            throw std::runtime_error(
+                "gstreamer_file_source: seeking from end of file impossible - could not query duration" );
+        }
+        newPosition = end + off;
+    }
 
-  return m_position;
+    if ( m_position != newPosition )
+    {
+        m_position = newPosition;
+        m_ring_buffer->flush();
+
+        if ( !gst_element_seek_simple( m_pipeline.get(),
+                                       GST_FORMAT_BYTES,
+                                       ( GstSeekFlags )( GST_SEEK_FLAG_FLUSH | GST_SEEK_FLAG_ACCURATE ),
+                                       m_position ) )
+        {
+            throw std::runtime_error( "gstreamer_file_source: seeking failed" );
+        }
+    }
+
+    return m_position;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::streamsize gstreamer_file_source::read(char* dst, std::streamsize numBytesRequested)
+std::streamsize gstreamer_file_source::read( char* dst, std::streamsize numBytesRequested )
 {
-  return recursive_read(dst, numBytesRequested);
+    return recursive_read( dst, numBytesRequested );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::streamsize gstreamer_file_source::recursive_read(char* dst, std::streamsize numBytesRequested)
+std::streamsize gstreamer_file_source::recursive_read( char* dst, std::streamsize numBytesRequested )
 {
-  auto read_bytes = m_ring_buffer->pull(dst, numBytesRequested);
+    auto read_bytes = m_ring_buffer->pull( dst, numBytesRequested );
 
-  dst += read_bytes;
-  numBytesRequested -= read_bytes;
-  m_position += read_bytes;
+    dst += read_bytes;
+    numBytesRequested -= read_bytes;
+    m_position += read_bytes;
 
-  if(numBytesRequested)
-  {
-    tGstPtr<GstElement> sink(gst_bin_get_by_name(GST_BIN(m_pipeline.get()), "sink"), gst_object_unref);
-    GstAppSink *app_sink = reinterpret_cast<GstAppSink *>(sink.get());
-
-    tGstPtr<GstSample> sample(gst_app_sink_pull_sample(app_sink), (GUnref) gst_sample_unref);
-
-    if(sample)
+    if ( numBytesRequested )
     {
-      auto buffer = gst_sample_get_buffer(sample.get());
+        tGstPtr<GstElement> sink( gst_bin_get_by_name( GST_BIN( m_pipeline.get() ), "sink" ), gst_object_unref );
+        GstAppSink*         app_sink = reinterpret_cast<GstAppSink*>( sink.get() );
 
-      GstMapInfo mapped;
+        tGstPtr<GstSample> sample( gst_app_sink_pull_sample( app_sink ), (GUnref) gst_sample_unref );
 
-      if(gst_buffer_map(buffer, &mapped, GST_MAP_READ))
-      {
-        auto for_now = std::min<gsize>(mapped.size, numBytesRequested);
-        std::copy(mapped.data, mapped.data + for_now, dst);
-        read_bytes += for_now;
-        dst += for_now;
-        numBytesRequested -= for_now;
-        m_position += for_now;
+        if ( sample )
+        {
+            auto buffer = gst_sample_get_buffer( sample.get() );
 
-        m_ring_buffer->push((char*) mapped.data + for_now, mapped.size - for_now);
-        gst_buffer_unmap(buffer, &mapped);
+            GstMapInfo mapped;
 
-        return read_bytes + recursive_read(dst, numBytesRequested);
-      }
+            if ( gst_buffer_map( buffer, &mapped, GST_MAP_READ ) )
+            {
+                auto for_now = std::min<gsize>( mapped.size, numBytesRequested );
+                std::copy( mapped.data, mapped.data + for_now, dst );
+                read_bytes += for_now;
+                dst += for_now;
+                numBytesRequested -= for_now;
+                m_position += for_now;
+
+                m_ring_buffer->push( (char*) mapped.data + for_now, mapped.size - for_now );
+                gst_buffer_unmap( buffer, &mapped );
+
+                return read_bytes + recursive_read( dst, numBytesRequested );
+            }
+        }
     }
-  }
 
-  return read_bytes;
+    return read_bytes;
 }
-
--- audiostream/src/ni/media/audio/source/gstreamer_file_source.h
+++ audiostream/src/ni/media/audio/source/gstreamer_file_source.h
@@ -27,60 +27,64 @@
 #include <boost/iostreams/categories.hpp>
 #include <boost/iostreams/positioning.hpp>
 
-#include <ni/media/audio/ifstream_info.h>
 #include <gst/gst.h>
+#include <ni/media/audio/ifstream_info.h>
 
 namespace detail
 {
-  template<typename T, size_t s> struct RingBuffer;
+template <typename T, size_t s>
+struct RingBuffer;
 }
 
 class gstreamer_file_source
 {
-  public:
+public:
     //----------------------------------------------------------------------------------------------------------------------
 
     using offset_type = boost::iostreams::stream_offset;
 
-    using FrameRange = boost::icl::right_open_interval<offset_type>;
+    using FrameRange    = boost::icl::right_open_interval<offset_type>;
     using FrameRangeSet = boost::icl::interval_set<FrameRange::domain_type, std::less, FrameRange>;
 
-    template<typename MfType>
-      using MfTypePtr = std::unique_ptr<MfType, std::function<void( MfType* )>>;
+    template <typename MfType>
+    using MfTypePtr = std::unique_ptr<MfType, std::function<void( MfType* )>>;
 
     //----------------------------------------------------------------------------------------------------------------------
 
-    explicit gstreamer_file_source(const std::string& path, audio::ifstream_info::container_type container, size_t stream = 0);
+    explicit gstreamer_file_source( const std::string&                   path,
+                                    audio::ifstream_info::container_type container,
+                                    size_t                               stream = 0 );
     ~gstreamer_file_source();
 
     audio::ifstream_info info() const
     {
-      return m_info;
+        return m_info;
     }
 
-    std::streampos seek(offset_type, BOOST_IOS::seekdir );
+    std::streampos  seek( offset_type, BOOST_IOS::seekdir );
     std::streamsize read( char*, std::streamsize );
 
-    private:
-    static void init_gstreamer();
-    static pcm::runtime_format create_runtime_format(GstStructure* caps_struct);
-    static pcm::number_type gst_format_char_to_number_type(const gchar format);
+private:
+    static void                init_gstreamer();
+    static pcm::runtime_format create_runtime_format( GstStructure* caps_struct );
+    static pcm::number_type gst_format_char_to_number_type( const gchar format );
 
-    void setup_source(const std::string& path, audio::ifstream_info::container_type container);
-    GstElement* prepare_pipeline(const std::string& path);
-    void preroll_pipeline();
+    void setup_source( const std::string& path, audio::ifstream_info::container_type container );
+    GstElement* prepare_pipeline( const std::string& path );
+    void     preroll_pipeline();
     GstState wait_for_async_operation();
-    void fill_format_info(GstStructure *caps_struct, audio::ifstream_info::container_type container);
-    std::streamsize recursive_read(char* dst, std::streamsize numBytesRequested);
+    void fill_format_info( GstStructure* caps_struct, audio::ifstream_info::container_type container );
+    std::streamsize recursive_read( char* dst, std::streamsize numBytesRequested );
 
-    static void onPadAdded(GstElement* element, GstPad* pad, GstElement* sink);
+    static void onPadAdded( GstElement* element, GstPad* pad, GstElement* sink );
 
     audio::ifstream_info m_info;
-    using GUnref = void(*)(gpointer);
-    template<typename T> using tGstPtr = std::unique_ptr<T, GUnref>;
+    using GUnref = void ( * )( gpointer );
+    template <typename T>
+    using tGstPtr = std::unique_ptr<T, GUnref>;
     tGstPtr<GstElement> m_pipeline;
 
     using RingBuffer = detail::RingBuffer<char, 65536>;
     std::unique_ptr<RingBuffer> m_ring_buffer;
-    int64_t m_position = 0;
-  };
+    int64_t                     m_position = 0;
+};
